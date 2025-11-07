937e8fad5920ced00206d76f84b193ab4aa1620a
SWD STORE <muhaip2@gmail.com>
2025-11-08 02:20:34 +0700

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,21 +1,6 @@
 // src/utils.js
-// Util umum untuk performa & reliabilitas
-
-// fetch dengan timeout agar request tidak ngegantung
-export async function fetchWithTimeout(url, opts = {}, timeoutMs = 1500) {
-  const ctrl = new AbortController();
-  const id = setTimeout(() => ctrl.abort('timeout'), timeoutMs);
+export function runBg(ctx, promise) {
   try {
-    const res = await fetch(url, { ...opts, signal: ctrl.signal });
-    return res;
-  } finally {
-    clearTimeout(id);
-  }
+    ctx.waitUntil(Promise.resolve(promise).catch(() => {}));
+  } catch (_) { /* noop */ }
 }
-
-// jalankan promise di background tanpa menjegal response 200 OK
-export const runBg = (ctx, p) => {
-  try {
-    ctx.waitUntil(Promise.resolve(p));
-  } catch { /* noop */ }
-};
